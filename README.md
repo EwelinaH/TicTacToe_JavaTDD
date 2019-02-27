af9dab040092aa141ade77cacf7b2e756fb9ea09
Ewelina <ewelina@Ewelinas-MacBook-Pro.local>
2019-02-27 17:28:30 +0100

    play method refactoring

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 
 This is a simple TicTacToe game using Java and TDD.
 
-1. Requirement No.1
+Requirement No.1
 - whenXOutsideBoardThenRuntimeException
 - whenYOutsideBoardThenRuntimeException
 - whenOccupiedThenRuntimeException
+
+Requirement No.2
+- givenFirstTurnWhenNextPlayerThenX
+- givenLastTurnWasXWhenNextPlayerThen0
--- src/main/java/com/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/tddjava/ch03tictactoe/TicTacToe.java
@@ -5,18 +5,23 @@ public class TicTacToe {
     // table 3x3 with empty places
 
 
-    public void play(int x, int y){
-        if(x < 1 || x > 3){
+    public void play(int x, int y) {
+        checkAxis(x);
+        checkAxis(y);
+        setBox(x, y);
+    }
+    private void checkAxis(int axis) {
+        if (axis < 1 || axis > 3) {
             throw
                     new RuntimeException("Wartosc X wykracza poza plansze");
-        } else if (y < 1 || y > 3){
-            throw
-                    new RuntimeException("Wartosc Y wykracza poza plansze");
         }
+    }
+
+    private void setBox(int x, int y) {
         if (board[x - 1][y - 1] != '\0'){
             throw
                     new RuntimeException("Pole jest juz zajete!");
-        } else {
+    } else {
             board[x - 1][y - 1] = 'X';
         }
     }
