27c83d1e8128e8dc3981e3b4215954f707ea8e88
Ewelina <ewelina@Ewelinas-MacBook-Pro.local>
2019-02-27 17:14:04 +0100

    exceptions & simple tests

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 
 This is a simple TicTacToe game using Java and TDD.
+
+1. Requirement No.1
+- whenXOutsideBoardThenRuntimeException
+- whenYOutsideBoardThenRuntimeException
+- whenOccupiedThenRuntimeException
--- src/main/java/com/tddjava/ch03tictactoe/TicTacToe.java
+++ src/main/java/com/tddjava/ch03tictactoe/TicTacToe.java
@@ -1,4 +1,23 @@
 package com.tddjava.ch03tictactoe;
 
 public class TicTacToe {
+    private Character[][] board = {{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+    // table 3x3 with empty places
+
+
+    public void play(int x, int y){
+        if(x < 1 || x > 3){
+            throw
+                    new RuntimeException("Wartosc X wykracza poza plansze");
+        } else if (y < 1 || y > 3){
+            throw
+                    new RuntimeException("Wartosc Y wykracza poza plansze");
+        }
+        if (board[x - 1][y - 1] != '\0'){
+            throw
+                    new RuntimeException("Pole jest juz zajete!");
+        } else {
+            board[x - 1][y - 1] = 'X';
+        }
+    }
 }
